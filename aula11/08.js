10c4600d47de8ff1a284258d5344ae89074b2d7a
JenifferScarpini <128192490+jenifferScarpini@users.noreply.github.com>
2024-07-14 21:33:24 -0300

    condiçoes em js
    condiçoes

---
+++ aula11/08.js
@@ -0,0 +1,9 @@
+var vel = 12.5
+console.log(`a vel é ${vel}`)
+
+if (vel > 60) {
+    console.log('voce foi multado')
+
+}
+
+console.log('dirija usando cinto de segurança')
---
+++ aula11/09.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`vivendo em ${pais}`)
+
+if (pais == 'brasil'){
+    console.log('brasileiro')
+}else{
+    console.log('estrangeiro')
+}
