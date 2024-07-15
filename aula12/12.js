df1bb52ea8462ac422fd04dab737c1c9f2b3e974
JenifferScarpini <128192490+jenifferScarpini@users.noreply.github.com>
2024-07-14 21:50:38 -0300

    fazndo switch

---
+++ aula12/12.js
@@ -0,0 +1,27 @@
+var agora = new Date()
+var diasem = agora.getDay()
+
+switch(diasem) {
+    case 0:
+        console.log('domingo')
+        break
+    case 1:
+        console.log('segunda')
+        break
+    case 2: 
+        console.log('terça')
+        break
+    case 3:
+        console.log('quata')
+    case 4: 
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sábado')
+        break
+    default:
+        console.log('data invalida')
+}
